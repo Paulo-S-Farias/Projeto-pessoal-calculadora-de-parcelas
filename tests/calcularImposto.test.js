b97c54d648fcb3a45184d9fdd385ef584b84888b
Paulo s. Farias <159643226+Paulo-S-Farias@users.noreply.github.com>
2025-03-12 23:34:08 -0300

    Update calcularImposto.test.js

--- tests/calcularImposto.test.js
+++ tests/calcularImposto.test.js
@@ -1,4 +1,4 @@
-const calcularImposto = require('../src/CalcularImposto');
+const calcularImposto = require('../src/calcularImposto');
 
 test('Deve calcular o imposto corretamente', () => {
   expect(calcularImposto(1000, 15)).toBe(150);
